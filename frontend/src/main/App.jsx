07d6c823c2e5b46accef1831bee2dd1e44a41b5e
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 10:31:32 -0300

    feat: Configuring Routes

--- frontend/src/main/App.jsx
+++ frontend/src/main/App.jsx
@@ -1,16 +1,13 @@
-require("bootstrap/dist/css/bootstrap.min.css");
-require("font-awesome/css/font-awesome.min.css");
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
 
 import React from "react";
-import Todo from "../todo/todo";
-import About from "../about/about";
 import Menu from "../template/menu";
+import Routes from "./routes";
 
-const App = (props) => (
+export default (props) => (
   <div className="container">
     <Menu />
-    <Todo />
-    <About />
+    <Routes />
   </div>
 );
-export default App;
