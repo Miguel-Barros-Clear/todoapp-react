c7719019df3d66c383ed8b725772f9bf7fcc940e
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 10:38:35 -0300

    feat: PageHeader component

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,9 +1,27 @@
 import React from "react";
+import pageHeader from "../template/pageHeader";
 
 export default (props) => {
   return (
     <div>
-      <h1>Sobre</h1>
+      <pageHeader name="Sobre" small="Nós" />
+      <h2>Nossa Historia</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic neque
+        nostrum iusto eveniet a dignissimos. Est beatae provident nihil
+        consequuntur ipsam dignissimos odio asperiores porro! Atque labore iure
+        mollitia nemo?
+      </p>
+      <h2>Missão</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem,
+        error!
+      </p>
+      <h2>Imprensa</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque molestias
+        cum, iure distinctio neque expedita?
+      </p>
     </div>
   );
 };
--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import pageHeader from "../template/pageHeader";
 
 export default class Todo extends Component {
   render() {
     return (
       <div>
-        <h1>Todo</h1>
+        <h1>Tarefas</h1>
       </div>
     );
   }
