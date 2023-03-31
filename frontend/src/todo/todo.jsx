ee07e4ff937bfd056d1eff3b91f161c82496df77
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-03-31 11:42:14 -0300

    feat: add event and integration

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -2,13 +2,17 @@ import React, { Component } from "react";
 import PageHeader from "../template/pageHeader";
 import TodoForm from "./todoForm";
 import TodoList from "./todoList";
+import Axios from "axios";
+
+const URL = "http://localhost:3003/api/todos";
 
 export default class Todo extends Component {
   constructor(props) {
     super(props);
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleChange = this.handleChange.bind(this);
     this.state = { description: "", list: [] };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleAdd = this.handleAdd.bind(this);
   }
 
   handleChange(e) {
@@ -17,6 +21,8 @@ export default class Todo extends Component {
 
   handleAdd() {
     console.log(this.state.description);
+    const description = this.state.description;
+    Axios.post(URL, { description }).then((resp) => console.log("Funfante"));
   }
 
   render() {
